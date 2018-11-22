bcf430b5cf4719ee519c8e24543b5dcbd739eeaf
Agatha Rachmat <agatha.rachmat@gmail.com>
2018-11-22 09:59:19 +0700

    view controller

--- StoviTheStoryVideo/Controller/ViewController.swift
+++ StoviTheStoryVideo/Controller/ViewController.swift
@@ -10,11 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menuThumbnailTableView: UITableView!
+    var thumbnailImage : [UIImage] = [#imageLiteral(resourceName: "Choco Vanilla"),#imageLiteral(resourceName: "Cappucino"),#imageLiteral(resourceName: "ChocoFIllingnya EGI")]
+    var thumbnail : [UIImageView] = []
+    var story : Story?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        menuThumbnailTableView.delegate = self
+        menuThumbnailTableView.dataSource = self
+        CloudKitHelper().fetchStoryRecord{(stories) in
+            print(stories)}
     }
-
+    
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//
+//        storiesMap = StoriesMap()
+//        guard let storiesMap = storiesMap else {return}
+//        for story in storiesMap.dummyStories {
+//            let beautifulMorningAnnotation = StoriesAnnotation(coordinate: story.coordinate)
+//            beautifulMorningAnnotation.titleVideo = story.title
+//            beautifulMorningAnnotation.thumbnailVideo = story.thumbnail
+//
+//            storiesMapView.addAnnotation(beautifulMorningAnnotation)
+//        }
+//    }
 
 }
 
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return thumbnailImage.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "menuCell", for: indexPath) as! FrontMenuTableViewCell
+        
+        cell.thumbnailVideo.image = thumbnailImage[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       //
+    }
+    
+    
+}
