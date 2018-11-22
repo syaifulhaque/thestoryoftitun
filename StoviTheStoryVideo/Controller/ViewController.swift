0d4ef6f318d98a5ade9947d8273cb354ac837b5b
Agatha Rachmat <agatha.rachmat@gmail.com>
2018-11-22 17:26:11 +0700

    view Controller fetch complete

--- StoviTheStoryVideo/Controller/ViewController.swift
+++ StoviTheStoryVideo/Controller/ViewController.swift
@@ -11,32 +11,35 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var menuThumbnailTableView: UITableView!
-    var thumbnailImage : [UIImage] = [#imageLiteral(resourceName: "Choco Vanilla"),#imageLiteral(resourceName: "Cappucino"),#imageLiteral(resourceName: "ChocoFIllingnya EGI")]
-    var thumbnail : [UIImageView] = []
+
     var story : Story?
+    var stories: [Story] = []
     
+   // typealias onFetchReccordSuccess = ([Story]) -> Void
+    var ab :Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         menuThumbnailTableView.delegate = self
         menuThumbnailTableView.dataSource = self
         CloudKitHelper().fetchStoryRecord{(stories) in
-            print(stories)}
+          //  print(stories)
+            DispatchQueue.main.async {
+                for story in stories {
+                    self.stories.append(story)
+                    self.ab += 1
+                    self.menuThumbnailTableView.reloadData()
+
+                }
+              //  self.menuThumbnailTableView.reloadData()
+
+            }
+            print("stories \(stories[0].title)")
+
+
+        }
+        print("stories1 \(stories)")
     }
     
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//
-//        storiesMap = StoriesMap()
-//        guard let storiesMap = storiesMap else {return}
-//        for story in storiesMap.dummyStories {
-//            let beautifulMorningAnnotation = StoriesAnnotation(coordinate: story.coordinate)
-//            beautifulMorningAnnotation.titleVideo = story.title
-//            beautifulMorningAnnotation.thumbnailVideo = story.thumbnail
-//
-//            storiesMapView.addAnnotation(beautifulMorningAnnotation)
-//        }
-//    }
-
 }
 
 
@@ -45,20 +48,37 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return thumbnailImage.count
+        
+        print(ab)
+        return ab
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 224
+    }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "menuCell", for: indexPath) as! FrontMenuTableViewCell
-        
-        cell.thumbnailVideo.image = thumbnailImage[indexPath.row]
-        
+        CloudKitHelper().fetchStoryRecord{(stories) in
+            //  print(stories)
+            DispatchQueue.main.async {
+          
+                cell.titleLabel.text = stories[indexPath.row].title
+                cell.descriptionLabel.text = stories[indexPath.row].description
+                cell.thumbnailVideo.image = UIImage(named: stories[indexPath.row].thumbnail)
+              
+            }
+            print("stories \(stories[0].title)")
+
+        }
         return cell
+        
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-       //
+      //  self.performSegue(withIdentifier: "toDetail", sender: self)
+
     }
+    
     
     
 }
