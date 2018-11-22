d68030c78b09e276ca16a3580c7d7c79f5c548ca
Agatha Rachmat <agatha.rachmat@gmail.com>
2018-11-22 10:01:05 +0700

    clpudkit helper

--- StoviTheStoryVideo/Helper/CloudKitHelper.swift
+++ StoviTheStoryVideo/Helper/CloudKitHelper.swift
@@ -7,3 +7,64 @@
 //
 
 import Foundation
+import CloudKit
+import UIKit
+
+typealias onSaveSuccess = (CKRecord)-> Void // func onsavesucess data CKrecord with data Void
+typealias onFetchRecordSuccess = ([Story])->Void // void=()
+class CloudKitHelper {
+    
+    func createStoryRecord(story: Story){
+        let record = CKRecord(recordType: "Story")
+        record["title"] = story.title
+        record["image"] = story.thumbnail
+        record["description"] = story.description
+        
+    }
+    
+    func fetchStoryRecord(onfetchSucsess: @escaping onFetchRecordSuccess){
+        let container = CKContainer.default()
+        let database = container.publicCloudDatabase
+        // let records = CKRecord(recordType: "Story")
+        // let predicate = NSPredicate(value: true) // choose the data we want like below
+        let predicate = NSPredicate(format: "title BEGINSWITH %@", "Beautiful")
+        let query = CKQuery(recordType: "Story", predicate: predicate)
+        database.perform(query, inZoneWith: nil ) {(records,error) in
+            //    print(records)
+            
+            var stories:[Story] = []
+            //unwrapping
+            if let fetchedRecords = records{
+                for record in fetchedRecords{
+                    let story = self.convertRecordToStory(record: record)
+                    if let convertedStory = story{
+                        stories.append(convertedStory)
+                    }
+                }
+            }
+            onfetchSucsess(stories)
+            //            records?.forEach(<#T##body: (CKRecord) throws -> Void##(CKRecord) throws -> Void#>)
+            
+        }
+        print("end of func", #function)
+    }
+    
+    func convertRecordToStory(record: CKRecord)-> Story?{
+        guard let title = record["title"] as? String,
+            let description = record["description"] as? String,
+            let thumbnail = record["image"] as? String else {
+                return nil
+        }
+        do {
+            
+            let story = try Story(title: title ,thumbnail: thumbnail, description: description)
+            return story
+            
+        } catch  {
+            print(error.localizedDescription)
+        }
+        
+        return nil
+    }
+    
+}
